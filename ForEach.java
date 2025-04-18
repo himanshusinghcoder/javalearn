2ca9f72ad269ac15ffa6ed07f6a28508de41e4b7
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2025-04-18 16:53:45 +0530

    some java basics are done

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] arg){
+        String[] cars = {"Volve", "BMW", "Lexus"};
+        for (String i : cars){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] arg){
+        for(int i = 0;i < 10;i++){
+            System.out.println(i);            
+        }
+    }
+}
